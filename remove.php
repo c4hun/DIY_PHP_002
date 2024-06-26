04e35a6b53c0c09563be155457ebc3c3eeeb8480
Chew <101428141+c4hun@users.noreply.github.com>
2024-06-26 16:40:47 +0200

    Update remove.php

--- remove.php
+++ remove.php
@@ -17,7 +17,7 @@ if($executeIsOk){
 ?>
 
 <!DOCTYPE html>
-<html lang="efr">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
